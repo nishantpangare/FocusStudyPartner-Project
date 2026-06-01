4a3343728ca2cd4f99114f98d280e43a5adfefab
guzidecalayir <guca00002@stud.uni-saarland.de>
2026-06-01 17:01:20 +0200

    Buzzer code added.

---
+++ Interactive_systems/src/buzzer.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+
+#define BUZZER_PIN 1
+
+int melody[] = {659, 659, 0, 659, 0, 523, 659, 0};
+int durations[] = {125, 125, 125, 125, 125, 125, 250, 125};
+int noteCount = 8;
+
+unsigned long startTime;
+unsigned long playDuration = 3000; // 5 saniye
+
+bool finished = false;
+
+void setup() {
+  pinMode(BUZZER_PIN, OUTPUT);
+  startTime = millis();
+}
+
+void loop() {
+  if (finished) {
+    noTone(BUZZER_PIN);
+    return;
+  }
+
+  for (int i = 0; i < noteCount; i++) {
+
+    // süre doldu kontrolü
+    if (millis() - startTime >= playDuration) {
+      noTone(BUZZER_PIN);
+      finished = true;
+      return;
+    }
+
+    // nota çal
+    if (melody[i] == 0) {
+      noTone(BUZZER_PIN);
+    } else {
+      tone(BUZZER_PIN, melody[i]);
+    }
+
+    delay(durations[i]);
+    noTone(BUZZER_PIN);
+    delay(30);
+  }
+}
